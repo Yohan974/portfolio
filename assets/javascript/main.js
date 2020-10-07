0694892167dfaf60ddcdd3e27e3d31b45134f9fe
Yohan <yohan.fontaine87@gmail.com>
2020-10-07 03:10:44 +0200

    suppression webpack

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,4 +1,3 @@
-import "./style.css";
 // Function expression to select elements
 const selectElement = (s) => document.querySelector(s);
 const navLinks = document.querySelectorAll(".nav-link");
@@ -13,7 +12,8 @@ selectElement(".burger-menu-icon").addEventListener("click", () => {
     if (link.style.animation) {
       link.style.animation = "";
     } else {
-      link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5
+      link.style.animation = `navLinkFade 0.5s ease forwards ${
+        index / 7 + 0.5
         }s`;
     }
   });
@@ -33,7 +33,8 @@ navLinks.forEach((link) => {
       if (link.style.animation) {
         link.style.animation = "";
       } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
           }s`;
       }
     });
